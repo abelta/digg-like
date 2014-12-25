badf1ff4fd02e98f52684a51391dec655c41633f
Abel Tamayo <abel.tamayo@gmail.com>
2014-12-25 12:11:14 +0100

    Tests user registration.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+    devise_for :users , controllers: { confirmations: "users/confirmations", passwords: "users/passwords", registrations: "users/registrations", sessions: "users/sessions", unlocks: "users/unlocks" }
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
     root 'home#index'
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
